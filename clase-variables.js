3c57868bc9bdc2e9d11c03f2f8e0b6824498f275
Isaac Garcia <garciaalejoisaac@gmail.com>
2024-09-26 19:07:21 -0500

    variable

---
+++ clase-variables.js
@@ -0,0 +1,24 @@
+let cajaDeAndy = 'Woody'
+console.log(cajaDeAndy)
+
+// Lo NO permitido 
+// let c = 'woody'
+// let cda = 'woody'
+// let pcAndy = 'woody'
+
+
+// Lo permitido 
+let primerTrasteoDeAndy = 'woody'
+let urlDelUsuario = 'https://www.google.com'
+let idDelUsuario = '1234567891012'
+
+console.log(primerTrasteoDeAndy)
+console.log(urlDelUsuario)
+console.log(idDelUsuario)
+
+let contador 
+contador = contador + 1
+console.log(contador)
+
+const pi = 3.14
+console.log(pi)
