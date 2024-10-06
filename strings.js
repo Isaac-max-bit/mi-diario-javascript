b6d2652547ff4bcbb1c7f42e09ed4050a132a616
Isaac Garcia <garciaalejoisaac@gmail.com>
2024-10-06 18:01:02 -0500

    string

---
+++ strings.js
@@ -0,0 +1,17 @@
+// Tipo de dato: string
+
+// let string1 = 'Hola, mundo '
+// let string2 = "Mortal Kombat 11 es mucho mejor que el 1"
+// let string3 = `${string1} feliz :)`
+// let string4 = string1 + ' ' + string2
+
+// console.log(string1)
+// console.log(string2)
+// console.log(string3)
+// console.log(string4)
+
+let frase = 'Existo luego existo'
+console.log(frase.length)
+console.log(frase.toLocaleLowerCase())
+console.log(frase.toLocaleUpperCase())
+console.log(frase.substring(0, 10))
