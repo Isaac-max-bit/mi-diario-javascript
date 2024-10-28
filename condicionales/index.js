ad81bbc124107977167cf2c589bb8f1c186b0524
Isaac Garcia <garciaalejoisaac@gmail.com>
2024-10-27 20:08:40 -0500

    Avance 27/10/2024

---
+++ condicionales/index.js
@@ -0,0 +1,9 @@
+let name = "Brayan";
+
+if(name === "Isaac"){
+    console.log("Que tal Isaac?");
+}else if (name === "Brayan") {
+    console.log("Hola Brayan");
+} else {
+    console.log("No eres Isaac");
+}
---
+++ condicionales/juego.js
@@ -0,0 +1,15 @@
+const numeroSecreto = Math.floor(Math.random() * 10 + 1);
+const numeroJugador = parseInt(prompt("Adivina el número secreto entre 1 y 10: "));
+
+console.log(`Este es el número con el que juegas: ${numeroJugador}`);
+
+if (isNaN(numeroJugador) || numeroJugador < 1 || numeroJugador > 10) {
+    console.log("Por favor, ingresa un número válido entre 1 y 10.");
+} else if (numeroJugador === numeroSecreto) {
+    console.log("¡Felicidades, adivinaste el número secreto!");
+} else if (numeroJugador < numeroSecreto) {
+    console.log("El número es demasiado bajo, intenta de nuevo.");
+} else {
+    console.log("El número es muy alto, intenta de nuevo."); 
+}  
+  
